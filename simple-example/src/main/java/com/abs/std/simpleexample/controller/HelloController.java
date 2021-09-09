6b17fd9d304f2fb6013de8ce42c92f45bec8754d
osipovvu <osipov.v.u@yandex.ru>
2021-09-09 13:51:19 +0300

    docker

--- simple-example/src/main/java/com/abs/std/simpleexample/controller/HelloController.java
+++ simple-example/src/main/java/com/abs/std/simpleexample/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.abs.std.simpleexample.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
 public class HelloController {
 
+    @Value("${hello.message}")
+    @Autowired
+    private String message;
+
     @GetMapping
     @ResponseBody
     public String hello() {
-        return "Hello";
+        return message;
     }
 
 }
